c23e3711a595287b3d811575837a6a0a8a30cdab
Andre Van Dyk <1andrevandyk@gmail.com>
2025-02-09 10:18:49 +0200

    Restarted Chat Page

--- app/dashboard/v/chats/page.tsx
+++ app/dashboard/v/chats/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
-import { ChatPageClient } from "./components/ChatPageCLient"
+import { ChatPageClient } from "./components/ChatPage"
 
 export default async function ChatsPage() {
   const supabase = createClient()
