84979ac1ae50a9597c623d747ccafcd693966cd8
andrevdyk <1andrevandyk@gmail.com>
2026-03-18 03:06:21 +0200

    had to add a suspense

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import DashboardSideBar from "./components/dashboard-side-bar";
 import DashboardTopNav from "./components/dashbord-top-nav";
 import { AssetFilterProvider } from "./components/asset-filter-context";
@@ -9,17 +9,17 @@ export default function DashboardLayout({
   children: ReactNode;
 }) {
   return (
-    <div className="grid min-h-screen lg:grid-cols-[40px_1fr] ">
+    <div className="grid min-h-screen lg:grid-cols-[40px_1fr]">
       <div className="static z-20">
         <DashboardSideBar />
       </div>
-      <DashboardTopNav>
-        <main className="flex flex-col gap-4 pl-4 pr-2 h-full lg:gap-6">
-        <AssetFilterProvider>{children}</AssetFilterProvider>
-        </main>
-      </DashboardTopNav>
+      <Suspense fallback={null}>
+        <DashboardTopNav>
+          <main className="flex flex-col gap-4 pl-4 pr-2 h-full lg:gap-6">
+            <AssetFilterProvider>{children}</AssetFilterProvider>
+          </main>
+        </DashboardTopNav>
+      </Suspense>
     </div>
   );
 }
-
-
